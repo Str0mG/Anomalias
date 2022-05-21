3505786153fdd80930292ee6b6e3d5638e597f3b
Str0mG <gabrieltrombini793@gmail.com>
2022-05-20 21:25:12 -0300

    add: file convet videos to frames

--- index.py
+++ index.py
@@ -5,12 +5,12 @@ import numpy as np
 from matplotlib import pyplot as plt
 from statistics import mean
 from math import sqrt
+import pandas as pd
 
 def humanSort(text):  # Sort function for strings w/ numbers
     convText = lambda seq: int(seq) if seq.isdigit() else seq.lower()
     arrayKey = lambda key: [convText(s) for s in re.split('([0-9]+)', key)]  # Split numbers and chars, base function for sorted
     return sorted(text, key=arrayKey)
-
 
 def read_file(source):
     result = []
@@ -33,14 +33,16 @@ def distE(a,b):
 
 def rgb(image):
     image = cv2.imread(image)
-    r = cv2.calcHist([image], [2], None, [256], [0, 256])
+    r = cv2.calcHist([image], [0], None, [256], [0, 256])
     return r
 
 
+# Verificar com mais frames para ver
 def normalize(root_csv, root_videos):
     i=0
 
     #Funcção para colocar cada linha do csv  numa lista
+    # Colocar numa lista de lista
     for csv_file in humanSort(os.listdir(root_csv)):
         i+=1
         print(humanSort(os.listdir(root_csv)))
@@ -56,30 +58,26 @@ def normalize(root_csv, root_videos):
     frameTrue = []
     frameFalse = []
     for i in range(len(framesAnotados)):
+        # Mudar para seu diretorio correto
+        r= rgb(f'/home/trombini/anomalias/projeto1/frames/fight/F_67_1_0_0_1.mp4/{framesImagem[i]}')
+        aux = []
+        aux = np.append(aux,r)
+        frameTrue.append(aux.tolist()) #lista de lista para vetores de cada frame [[],[]...]
         
-        if '1' in framesAnotados[i]:
-            
-            r= rgb(f'/home/trombini/anomalias/projeto1/fight/F_1_0_1_0_1.mp4/{framesImagem[i]}')
-            aux = []
-            aux = np.append(aux,r)
-            frameTrue.append(aux.tolist()) #lista de lista para vetores de cada frame [[],[]...]
         
-        if '0' in framesAnotados[i]:
-            r= rgb(f'/home/trombini/anomalias/projeto1/fight/F_1_0_1_0_1.mp4/{framesImagem[i]}')
-            aux = []
-            aux = np.append(aux,r)
-            frameFalse.append(aux.tolist()) #lista de lista para vetores de cada frame [[],[]...]
     
     #Calcular o vetor médio do R para lista de lista(Não precisa)
-    mediaTrue = [mean(values) for values in zip(*frameTrue)]
-    mediaFalse = [mean(values) for values in zip(*frameFalse)]
-    dist = np.linalg.norm(mediaTrue-mediaFalse)
+    # mediaTrue = [mean(values) for values in zip(*frameTrue)]
+    # mediaFalse = [mean(values) for values in zip(*frameFalse)]
+    # dist = np.linalg.norm(mediaTrue-mediaFalse)
     #return mediaTrue,mediaFalse,dist
 
     #Calcular a diferença entre cada frame (Detecção de movimento) len = len(frames - 1)
     delta = []
     for i in range(len(frameTrue)-1):
         delta.append(distE(frameTrue[i+1],frameTrue[i]))
+
+    return delta
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -89,11 +87,14 @@ if __name__ == '__main__':
     parser.add_argument('--root_videos',    type=str, default='PATH_TO_VIDEOS', help='Root path of your videos')
     
     opt = parser.parse_args()
+    # Mudar para seu diretorio
+    dif =normalize('/home/trombini/anomalias/projeto1/annotation','/home/trombini/anomalias/projeto1/frames/fight/F_67_1_0_0_1.mp4')
 
-    num_frames = opt.fps * opt.duration
+    # transforma para csv
+    df = pd.DataFrame(dif,columns=['Diferenca'])
 
-    a,b,c =normalize('/home/trombini/anomalias/projeto1/annotation','/home/trombini/anomalias/projeto1/fight/F_1_0_1_0_1.mp4')
-    print(a,b,c)
+    df.to_csv('Eae2.csv',index=False)
+
 
 
 
